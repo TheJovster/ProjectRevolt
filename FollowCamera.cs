475667653246401e51f2730bd83441c12cdca7c0
TheJovster <joca.251@hotmail.com>
2022-10-08 15:25:28 +0200

    Added Follow Cam - 15:25 - 08/10/22
    Added a very basic follow cam. Will change to cinemachine later.

---
+++ FollowCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 cameraOffset;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = target.position + cameraOffset;
+    }
+}
--- Mover.cs
+++ Mover.cs
@@ -20,7 +20,6 @@ public class Mover : MonoBehaviour
         if(Input.GetMouseButtonDown(0)) 
         {
             MoveToCursor();
-            
         }
 
     }
