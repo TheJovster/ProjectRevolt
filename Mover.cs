bdf82ae79d799102d050d76608c803c6ec6cd016
TheJovster <joca.251@hotmail.com>
2022-10-08 16:20:46 +0200

    Update Mover.cs

--- Mover.cs
+++ Mover.cs
@@ -45,6 +45,7 @@ public class Mover : MonoBehaviour
     {
         Vector3 velocity = navMeshAgent.velocity;
         Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        animator.SetFloat("MoveSpeed", localVelocity.magnitude, 0.1f, Time.deltaTime); //this does work
+        float moveSpeed = localVelocity.z;
+        animator.SetFloat("MoveSpeed", moveSpeed, 0.1f, Time.deltaTime); //this does work
     }
 }
