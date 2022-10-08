8f84aa4f88f90036fbe46ef403f3fc007a4cf458
TheJovster <joca.251@hotmail.com>
2022-10-08 11:51:17 +0200

    Updated Mover.cs
    Added basic movement using NavMesh

--- Mover.cs
+++ Mover.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform target;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        NavMeshAgent agent = this.GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
     }
 }
