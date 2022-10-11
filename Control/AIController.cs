df6e6cc227fe709a9f4700cd67195e3dce71b567
TheJovster <joca.251@hotmail.com>
2022-10-11 08:59:25 +0200

    AIController.cs - basic layout and pseudocode
    Started implementing chase behaviours in AIController.cs.

--- Control/AIController.cs
+++ Control/AIController.cs
@@ -6,14 +6,44 @@ namespace ProjectRevolt.Control
 {
     public class AIController : MonoBehaviour
     {
+        //variables
+        [SerializeField] private float chaseDistance;
+
+        //components
+
+        //game objects
+
         void Start()
         {
-
+            
         }
 
         void Update()
         {
+            if (DistanceToPlayer() <= chaseDistance)
+            {
+                Debug.Log(this.name + " should give chase.");
+                //add chase behaviour here
+            }
+        }
 
+        //behaviour methods
+
+        //guard behaviour
+
+        //patrol behaviour
+
+        //suspicion behaviour
+
+        //chase behaviour
+
+        //attack/aggression behaviour
+
+        //calculations
+        private float DistanceToPlayer() 
+        {
+            Transform player = GameObject.FindWithTag("Player").transform;
+            return Vector3.Distance(player.position, transform.position);
         }
     }
 
