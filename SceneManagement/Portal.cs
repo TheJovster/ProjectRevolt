9cf1c561ea504bf8881f0c4ea138138ba91f5cdc
TheJovster <joca.251@hotmail.com>
2022-10-14 12:58:06 +0200

    Updated Portal.cs
    Added PlayerSpawnPoint location.

--- SceneManagement/Portal.cs
+++ SceneManagement/Portal.cs
@@ -8,6 +8,7 @@ namespace ProjectRevolt.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int sceneToLoad = -1;
+        public Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -20,7 +21,7 @@ namespace ProjectRevolt.SceneManagement
                 else if(sceneToLoad == 0) 
                 {
                     Debug.Log("You set out to the Burned Village");
-                }
+                }//ignore this - it's just for debugging
                 
                 StartCoroutine(Transition());
             }
@@ -30,8 +31,31 @@ namespace ProjectRevolt.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            Debug.Log("Scene Loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal() 
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>()) 
+            {
+                if (portal == this)
+                {
+                    continue;
+                }
+                else return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal) 
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
     }
 }
