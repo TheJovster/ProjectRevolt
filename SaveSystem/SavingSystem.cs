06c441fe2573d42f6cf39d7babad7e7b8dcca258
TheJovster <joca.251@hotmail.com>
2022-10-17 12:58:39 +0200

    Updared SavingSystem - added basic serialization capability to it
    The system can save and store basic text files now. The files remain persistent between sessions.

--- SaveSystem/SavingSystem.cs
+++ SaveSystem/SavingSystem.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 namespace ProjectRevolt.Saving 
 {
@@ -8,12 +10,31 @@ namespace ProjectRevolt.Saving
     {
         public void Save(string saveFile) 
         {
-            Debug.Log("Saving to " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            Debug.Log("Saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create)) 
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("This is a text message."); //serializes and encodes data
+                stream.Write(bytes, 0, bytes.Length);
+            }
         }
 
         public void Load(string saveFile) 
         {
-            Debug.Log("Loading from " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            Debug.Log("Loading from " + path);
+            using(FileStream stream = File.Open(path, FileMode.Open)) 
+            {
+                byte[] byteBuffer = new byte[stream.Length];
+                stream.Read(byteBuffer, 0, byteBuffer.Length); //de-serializes and decodes data (reads it)
+                Debug.Log(Encoding.UTF8.GetString(byteBuffer));
+            }
+        }
+
+        private string GetPathFromSaveFile(string saveFile) 
+        {
+            
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
 }
