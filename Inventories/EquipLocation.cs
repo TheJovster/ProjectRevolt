a8cd228653c84191c860c63b375eb2ed66cc2b94
TheJovster <joca.251@hotmail.com>
2022-11-06 01:30:08 +0100

    Entire Inventory system entered
    The inventory code has been integrated.

---
+++ Inventories/EquipLocation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectRevolt.Inventories
+{
+    /// <summary>
+    /// Locations on the players body where items can be equipped.
+    /// </summary>
+    public enum EquipLocation
+    {
+        Helmet,
+        Necklace,
+        Body,
+        Trousers,
+        Boots,
+        Weapon,
+        Shield,
+        Gloves,
+    }
+}
