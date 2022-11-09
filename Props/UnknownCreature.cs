2754a63400d19594a4cddaa3ba592bef69d519ff
TheJovster <joca.251@hotmail.com>
2022-11-09 05:38:05 +0100

    A tiny addition to the TinyCreature script

--- Props/UnknownCreature.cs
+++ Props/UnknownCreature.cs
@@ -1,17 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using ProjectRevolt.Attributes;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnknownCreature : MonoBehaviour
 {
     [SerializeField] private AudioClip[] soundFX;
     [SerializeField] private AudioSource audioSource;
-    
+    [SerializeField] private GameObject healFX;
+    [SerializeField] private GameObject leftEye;
+    [SerializeField] private GameObject rightEye;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player") 
         {
             audioSource.PlayOneShot(soundFX[Random.Range(0, soundFX.Length)]);
+            other.gameObject.GetComponent<Health>().Heal(25f);
+            GameObject healFXInstance = Instantiate(healFX, other.transform.position + Vector3.up, Quaternion.identity);
+            Destroy(healFXInstance, .75f);
+            leftEye.SetActive(false);
+            rightEye.SetActive(false);
+            GetComponent<Collider>().enabled = false;
+            
         }
     }
 }
